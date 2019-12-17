f58cef42830342c353eb026c69fe2c64c3009ff2
Jacob Bowdoin <jwbowdoin@gmail.com>
2019-12-17 13:55:05 -0800

    Comment stats

--- dist/index.js
+++ dist/index.js
@@ -392,19 +392,16 @@ async function run() {
     const sha = process.env.GITHUB_SHA;
     core.debug(`SHA: ${sha}`);
 
-    console.log(`about to read: ${inputs.path}`)
-    console.log(`about to read: ${process.env.GITHUB_WORKSPACE}/${inputs.path}`)
-    const data = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/coverage/coverage-summary.json`, 'utf8');
+    console.log(`about to read: ${process.env.GITHUB_WORKSPACE}/`)
+    const data = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/${inputs.path}`, 'utf8');
     const json = JSON.parse(data);
     console.log(json);
-    
-    fs.readdir(process.env.GITHUB_WORKSPACE, function (err, items) {
-      console.log(items);
 
-      for (var i = 0; i < items.length; i++) {
-        console.log(items[i]);
-      }
-    });
+    const coverage = `==== **Test Coverage** ====
+Statements: ${json.total.statements.pct}% ( ${json.total.statements.covered}/${json.total.statements.total} )
+Branches  : ${json.total.branches.pct}% ( ${json.total.branches.covered}/${json.total.branches.total} )
+Functions : ${json.total.functions.pct}% ( ${json.total.functions.covered}/${json.total.functions.total} )
+Lines     : ${json.total.lines.pct}% ( ${json.total.lines.covered}/${json.total.lines.total} )`
 
     await request(
       `POST /repos/${process.env.GITHUB_REPOSITORY}/commits/${sha}/comments`,
@@ -412,7 +409,7 @@ async function run() {
         headers: {
           authorization: `token ${inputs.token}`
         },
-        body: eval('`' + inputs.path + '`')
+        body: eval('`' + coverage + '`')
       }
     );
   } catch (error) {
