91bcd3f916c1652fa51284ac2bd4d68b37f4ad37
Jacob Bowdoin <jwbowdoin@gmail.com>
2019-12-17 16:05:59 -0800

    Add logging

--- dist/index.js
+++ dist/index.js
@@ -523,6 +523,15 @@ async function run() {
 
     const data = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/${inputs.path}`, 'utf8');
     const json = JSON.parse(data);
+    console.log(json);
+
+    fs.readdir(process.env.GITHUB_WORKSPACE, function (err, items) {
+      console.log(items);
+
+      for (var i = 0; i < items.length; i++) {
+        console.log(items[i]);
+      }
+    });
 
     const coverage = `==== **Test Coverage** ====
 Statements: ${json.total.statements.pct}% ( ${json.total.statements.covered}/${json.total.statements.total} )
